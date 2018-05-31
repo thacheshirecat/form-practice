f043b2166a70f4ef33fcc93c5a442eab018bd51c
Cheshire Cat <thacheshirecat@gmail.com>
2018-05-30 20:02:44 -0700

    Add initial HTML to form-letter, as well as initial JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,45 @@
 $(document).ready(function() {
 
 $("#formOne").submit(function(event) {
-  var userinputValue = $("input#user-input").val();
-  var louder = userinputValue.toUpperCase();
-  $(".user-input").text(louder);
+  var userNameInpute = $("inpute#userName").val()
+  // var style = userNameInpute.addClass("fancy")
+  $(".userName").text(userNameInpute);
+
+  $("#letter").show();
 
   event.preventDefault();
-});
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+// $("#formOne").submit(function(event) {
+//   var userinputValue = $("input#user-input").val();
+//   var louder = userinputValue.toUpperCase();
+//   $(".user-input").text(louder);
 //
-// mad lib scripts (disabled)
+//   event.preventDefault();
+// });
 //
-
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
-});
+// //
+// // mad lib scripts (disabled)
+// //
+//
+//   $("#formOne").submit(function(event) {
+//     var person1Input = $("input#person1").val();
+//     var person2Input = $("input#person2").val();
+//     var animalInput= $("input#animal").val();
+//     var exclamationInput = $("input#exclamation").val();
+//     var verbInput = $("input#verb").val();
+//     var nounInput = $("input#noun").val();
+//
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+//
+//     $("#story").show();
+//
+//     event.preventDefault();
+//   });
+// });
